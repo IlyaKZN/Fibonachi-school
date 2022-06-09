21c518a4bd56bb48e7c6eed60853d26f3f8f6ce9
Ilya Pichugin <ilia-pichugin2014@yandex.ru>
2022-06-10 01:39:27 +0300

    реализованы страницы стэка и очереди

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,10 +1,164 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from './styles.module.css';
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { RadioInput } from "../ui/radio-input/radio-input";
+import { Button } from "../ui/button/button";
+import { Direction } from "../../types/direction";
 
 export const SortingPage: React.FC = () => {
+
+  const [arr, setArr] = useState<{ value: number, color: string, columnHeight: number }[]>([]);
+
+  const randomArr = () => {
+    const resArr = [];
+
+    const maxValue = 100;
+    const minValue = 1;
+
+    const minLengthArr = 3;
+    const maxLengthArr = 17;
+
+    const lengthArr = Math.floor(Math.random() * (maxLengthArr - minLengthArr + 1) + minLengthArr);
+
+    for (let i = 0; i < lengthArr; i++) {
+      const value = Math.floor(Math.random() * (maxValue - minValue + 1) + minValue)
+      resArr.push({
+        value: value, 
+        color: '#0032FF', 
+        columnHeight: (340 * value) / 100
+      })
+    }
+
+    setArr(resArr);
+  };
+
+  const swap = (arr: { value: number, color: string, columnHeight: number }[], firstIndex: number, secondIndex: number): void => {
+    const temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+  };
+
+  async function sort () {
+    const sortableArray = arr;
+    const length = sortableArray.length;
+    async function cycle (i: number) {
+      if (i < length) {
+        let maxInd = i;
+
+        //@ts-ignore
+        async function findMax (j: number) {
+
+          await new Promise(resolve => {
+            setTimeout(() => {
+              console.log(j)
+              if (j < length) {
+                
+                if (sortableArray[j].value < sortableArray[maxInd].value) {
+                  maxInd = j;
+                }
+                
+                findMax(j + 1);
+                
+              }
+            }, 200);
+          });
+        }
+
+        let res = await findMax(i);
+
+        console.log(res)
+        
+        if (maxInd !== i) {
+          swap(sortableArray, maxInd, i)
+        }
+        // cycle(i + 1);
+
+      }
+      setArr([...sortableArray]);
+      
+    }
+    cycle(0);
+  }
+
+  // const sort1 = () => {
+  //   const sortableArray = arr;
+  //   function externalDelay (i: number) {
+  //     if (i < sortableArray.length) {
+  //       sortableArray[i].color = '#D252E1';
+  //       // setTimeout(function() {
+  //         console.log('Внешний :',sortableArray)
+  //         function internalDelay (j: number) {
+  //           if (j < sortableArray.length) {
+  //             setTimeout(function() {
+  //               console.log('Внутренний :', sortableArray)
+
+  //               if (sortableArray[i].value > sortableArray[j].value) {
+  //                 swap(sortableArray, i, j)
+  //               }
+  //               internalDelay(j + 1);
+  //               setArr([...sortableArray]);
+  //               if (j === sortableArray.length - 1) {
+  //                 externalDelay(i + 1);
+  //               }
+  //             }, 500)
+  //           }
+  //         }
+          
+  //         internalDelay(0);
+          
+          
+  //       // },500)
+  //     }
+  //   }
+  //   externalDelay(0);
+  //   console.log(sortableArray)
+  //   setArr([...sortableArray]);
+  // };
+
+  // const sort = () => {
+  //   const sortableArray = arr;
+  //   for (let i = 0; i < sortableArray.length; i++) {
+  //     setTimeout(() => {
+  //       function internalDelay (j: number) {
+  //         if (j < sortableArray.length) {
+  //           setTimeout(function() {
+  //             if (sortableArray[i].value > sortableArray[j].value) {
+  //               swap(sortableArray, i, j)
+  //             }
+  //             internalDelay(j + 1);
+  //             setArr([...sortableArray]);
+  //           }, 500)
+  //         }
+  //       }
+  //       internalDelay(0)
+  //     }, 1000)
+  //   }
+  //   console.log(sortableArray)
+  //   setArr([...sortableArray]);
+  // };
+  
+
   return (
     <SolutionLayout title="Сортировка массива">
-
+      <div className={styles.container}>
+        <div className={styles.radioButtonsContainer}>
+          <RadioInput label="Выбор" name="sortMethod" value='Выбор' />
+          <RadioInput label="Пузырёк" name="sortMethod" value='Пузырёк' />
+        </div>
+        <div className={styles.methodButtonsContainer}>
+          <Button text="По возрастанию" sorting={Direction.Ascending} onClick={sort} />
+          <Button text="По убыванию" sorting={Direction.Descending} />
+        </div>
+        <Button onClick={randomArr} text="Новый массив" extraClass={styles.newArrayButton} />
+      </div>
+      <div className={styles.columnsContainer}>
+      {arr.map((el, index) => ( 
+        <div key={index}>
+          <div  style={{height: el.columnHeight, width: 50, backgroundColor: el.color}}></div>
+          <p className={styles.columnDescription}>{el.value}</p>
+        </div>
+      ))}
+      </div>
     </SolutionLayout>
   );
 };
--- src/components/stack-page/stack-page.tsx
+++ src/components/stack-page/stack-page.tsx
@@ -1,10 +1,102 @@
-import React from "react";
+import React, { useState, ChangeEvent, useEffect } from "react";
+import styles from "./styles.module.css";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Stack } from "./stack";
+import { Circle } from "../ui/circle/circle";
+import { ElementStates } from "../../types/element-states";
+
+type TStackValue = {
+  value: string,
+  state: ElementStates;
+}[];
 
 export const StackPage: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [stack, setStack] = useState<Stack<string>>();
+  const [stackValue, setStackValue] = useState<TStackValue>([]);
+
+  useEffect(() => {
+    const stack = new Stack<string>();
+    setStack(stack);
+  }, []);
+
+  const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const pushElement = () => {
+    if (value) {
+      stack!.push(value);
+      setValue("");
+      getElements({ startAnim: true });
+    };
+  };
+
+  const deleteElement = () => {
+    animation().then(() => {
+      console.log(stackValue);
+      stack!.pop();
+      getElements({ startAnim: false });
+    });
+  };
+
+  const cleareStack = () => {
+    stack!.clear();
+    getElements({ startAnim: false });
+  };
+
+  async function animation (stack: TStackValue = stackValue) {
+    const length = stack.length;
+    const lastEl = stack[length - 1];
+    stack[length - 1] = {...lastEl, state: ElementStates.Changing}
+    setStackValue([...stack]);
+    await new Promise((res, rej) => setTimeout(() => {
+      stack[length - 1] = {...lastEl, state: ElementStates.Default};
+      setStackValue([...stack]);
+      res('');
+    }, 500))
+  }
+
+  const getElements = ({ startAnim }: { startAnim: boolean }) => {
+    const elements = stack!.getElements();
+    const elementsData: TStackValue = elements.map((el) => ({ value: el, state: ElementStates.Default }));
+    setStackValue([...elementsData]);
+    if (startAnim) {
+      animation(elementsData);
+    }
+  };
+
   return (
     <SolutionLayout title="Стек">
-
+      <div className={styles.controlContainer}>
+        <div className={styles.container}>
+          <Input
+            value={value}
+            onChange={(e) => onChangeInput(e as ChangeEvent<HTMLInputElement>)}
+            extraClass={styles.input}
+            maxLength={4}
+            isLimitText={true}
+          />
+          <Button text="Добавить" onClick={pushElement} disabled={!value} />
+          <Button text="Удалить" onClick={deleteElement} disabled={!stackValue.length} />
+        </div>
+        <Button text="Очистить" onClick={cleareStack} disabled={!stackValue.length} />
+      </div>
+      <div className={styles.circlesContainer}>
+        {stackValue.length
+          ? stackValue.map((el, index) => (
+              <Circle
+                state={el.state}
+                letter={el.value}
+                key={index}
+                index={index}
+                head={index === stackValue.length - 1 ? "top" : null}
+              />
+            ))
+          : null}
+      </div>
     </SolutionLayout>
   );
 };
